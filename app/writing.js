f308fb14992ec9fb7c5bf056baf9e4358d36f94f
dan-nl <d_entous@yahoo.com>
2016-09-30 12:25:17 +0200

    added the creation of the test directory

--- app/writing.js
+++ app/writing.js
@@ -1,6 +1,25 @@
 'use strict';
 
+/**
+ * module dependencies
+ */
+var fs = require( 'fs' );
+var chalk = require( 'chalk' );
+
+/**
+ * @returns {void}
+ */
 function writing() {
+  try {
+    fs.mkdirSync( './test' );
+  } catch ( err ) {
+    if ( err.code === 'EEXIST' ) {
+      this.log( chalk.cyan( 'exists' ) + ' ./test' );
+    } else {
+      this.log( err.message );
+    }
+  }
+
   this.fs.extendJSON(
     './package.json',
     {
